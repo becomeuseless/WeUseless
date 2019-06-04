141f3de46b4763acc3ab840cc47613e32143d513
Koala <46984391+WOKALO@users.noreply.github.com>
2019-06-03 21:18:08 -0700

    Finished 01 Two Sum
    It is really good exercise.

---
+++ 01_Two_Sum_Thunder.py
@@ -0,0 +1,29 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+9-2 = 7
+
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = {}
+        result = []
+        for i in range(len(nums)):
+            for key,value in diff.items():
+                #print(str(key) + '-' + str(value))
+                if nums[i] == value and i != key:
+                    result.append(key)
+                    result.append(i)
+                    break
+            diff[i] = target - nums[i]
+        return result
